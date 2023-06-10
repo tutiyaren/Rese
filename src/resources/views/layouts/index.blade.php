e32fe3c48e18ec1f354ebdad6b2bed2c0960b402
ren <r999b66k222@outlook.jp>
2023-06-10 19:52:21 +0900

    favicon

--- src/resources/views/layouts/index.blade.php
+++ src/resources/views/layouts/index.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atte</title>
-    <link rel="icon" href="../../../favicon.ico" />
+    <link rel="icon" href="../../img/1554543.png" />
     <link rel="stylesheet" href="{{asset('css/sanitize.css')}}">
     <link rel="stylesheet" href="{{asset('css/index.css')}}">
     @yield('css')

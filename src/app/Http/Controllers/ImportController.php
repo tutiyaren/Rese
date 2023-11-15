b800cf0d0ef18b71177d7819ccf44a600957beff
ren <r999b66k222@outlook.jp>
2023-11-15 15:27:30 +0900

    import

--- src/app/Http/Controllers/ImportController.php
+++ src/app/Http/Controllers/ImportController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use App\Models\Shop;
 use App\Http\Requests\ImportRequest;
 use Illuminate\Support\Facades\Validator;
+use App\Models\Representative;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+
 
 class ImportController extends Controller
 {
@@ -24,6 +28,11 @@ class ImportController extends Controller
             if (!$existingStore) {
                 // バリデーション
                 $validator = Validator::make($storeData, [
+                    '管理者ID' => 'required',
+                    '店舗代表者名前' => 'required|max:255',
+                    '店舗代表者メールアドレス' => 'required|email',
+                    '今の日時' => '',
+                    'パスワード' => 'required|min:8|max:255|',
                     '店舗代表者ID' => 'required',
                     '店舗名' => 'required|max:50',
                     '地域' => 'required|in:東京都,大阪府,福岡県',
@@ -36,14 +45,69 @@ class ImportController extends Controller
                     // エラー処理
                     return redirect()->back()->withErrors($validator)->withInput();
                 }
+
+                if ($request->hasFile('画像URL')) {
+                    // アップロードされた画像ファイルを取得
+                    $uploadedImage = $request->file('画像URL');
+                    // 画像ファイル名を取得
+                    $imageName = $uploadedImage->getClientOriginalName();
+                    // 画像を storage/app/public ディレクトリに保存し、ファイル名は '画像URL' から取得
+                    $imagePath = $uploadedImage->storeAs('public', $imageName);
+                    // 保存した画像のURLを取得
+                    $imageUrl = Storage::url($imagePath);
+                    // 保存した画像のファイル名のみを取得
+                    $savedImageFileName = pathinfo($imageUrl, PATHINFO_BASENAME);
+                    // 画像の拡張子を変更（例えば .png に変更）
+                    $newImageName = pathinfo($savedImageFileName, PATHINFO_FILENAME) . '.png';
+                    Storage::move("public/{$savedImageFileName}", "public/{$newImageName}");
+                    // 保存した画像のURLを新しいファイル名で取得
+                    $imageUrl = Storage::url("public/{$newImageName}");
+                } else {
+                    // 画像URLが存在しない場合、CSV から取得
+                    $imageUrl = $storeData['画像URL'];
+                    // 画像をダウンロードして保存
+                    $imageContent = file_get_contents($imageUrl);
+                    $imageName = basename($imageUrl);
+                    Storage::put("public/{$imageName}", $imageContent);
+                    // 保存した画像のURLを取得
+                    $imageUrl = Storage::url($imageName);
+                    // 保存した画像のファイル名のみを取得
+                    $savedImageFileName = pathinfo($imageUrl, PATHINFO_BASENAME);
+                    // 画像の拡張子を変更（例えば .png に変更）
+                    $newImageName = pathinfo($savedImageFileName, PATHINFO_FILENAME) . '.png';
+                    Storage::move("public/{$savedImageFileName}", "public/{$newImageName}");
+                    // 保存した画像のURLを新しいファイル名で取得
+                    $imageUrl = Storage::url("public/{$newImageName}");
+                }
+
+                // 代表者を取得または新しく作成
+                $representative = Representative::firstOrNew(
+                    [
+                        'name' => $storeData['店舗代表者名前'],
+                        'email' => $storeData['店舗代表者メールアドレス'],
+                    ],
+                    [
+                        'email_verified_at' => $storeData['今の日時'],
+                    ]
+                );
+
+                // パスワードのハッシュ化
+                $hashedPassword = Hash::make($storeData['パスワード']);
+
+                // ハッシュ化したパスワードを設定
+                $representative->password = $hashedPassword;
+
+                $representative->save();
+
                 // データベースに保存
                 Shop::create([
-                    'representative_id' => $storeData['店舗代表者ID'],
+                    'representative_id' =>
+                    $representative->id,
                     'shop' => $storeData['店舗名'],
                     'area' => $storeData['地域'],
                     'genre' => $storeData['ジャンル'],
                     'content' => $storeData['店舗概要'],
-                    'image' => $storeData['画像URL'],
+                    'image' => $newImageName,
                 ]);
             }
         }
